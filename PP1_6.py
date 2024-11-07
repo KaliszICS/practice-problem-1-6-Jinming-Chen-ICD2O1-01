fda43f6db79c887210a878ddc500c5b239145ea3
Jinming-Chen-ICD2O1-01 <jinming.chen@student.tdsb.on.ca>
2024-11-07 09:57:00 -0500

    Update PP1_6.py

--- PP1_6.py
+++ PP1_6.py
@@ -1,36 +1,36 @@
 '''
 	File Name: errors.py
-	Author: Mr. Kalisz
-	Date Created: March 29, 2019
-	Date Last Edited: Sept 23, 2024
+	Author:Jinming Chen
+	Date Created: Nove 07, 2024
+	Date Last Edited: Nove 07, 2024
 '''
 
 # From input, recieve two integers from the user and add them together.  Output the result.
 def q1():
-  num1 = input("Input a number: ")
-  num2 = input("Input a number: ")
+  num1 = int("Input a number: ")
+  num2 = int("Input a number: ")
   
-  Print(num + num2)
+  print(num1 + num2)
 
 # From input recieve two integers.  Output the quotient rounded down.
 
 def q2():
-  num1 = input("Input a number: ")
-  num2 = input("Input a number: ")
+  num1 = int("Input a number: ")
+  num2 = int("Input a number: ")
   
-  print(num1/num2)
+  print(num1//num2)
 
 # Output the phrase "hello Mr. Kalisz have you seen my work yet?"
 
 def q3():
-  print (hello Mr. Kalisz have you seen my work yet?)
+  print ("hello Mr. Kalisz have you seen my work yet?")
 
 # From input recieve two numbers (can be decimal fractions).  
 # Output their result multiplied together.  Then round down to the nearest whole number
 
 def q4():
-  input("Input a number: ")
-  num2 = input("Input a number: ")
+  num1 = float("Input a number: ")
+  num2 = float("Input a number: ")
   
   print (int(num1 * num2))
 
